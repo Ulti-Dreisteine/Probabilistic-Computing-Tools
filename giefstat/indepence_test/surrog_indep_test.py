7c9c83a4d920444f8c0df92ecbe730fbba6e3157
dreisteine_thinkbook <dreisteine262@163.com>
2024-01-10 15:33:58 +0800

    add: 基于Bootstrap的独立性检验

--- giefstat/indepence_test/surrog_indep_test.py
+++ giefstat/indepence_test/surrog_indep_test.py
@@ -16,32 +16,56 @@ __doc__ = """
     计算关联系数并通过代用数据重采样获得结果的显著性信息
 """
 
+from typing import Tuple
+import numpy as np
 import logging
 import random
-import numpy as np
 
 from ..coefficient import cal_general_assoc
 
 
-def exec_surrog_indep_test(x, y, method, z=None, xtype=None, ytype=None, ztype=None, rounds=100, 
-                           alpha=0.05, max_size_bt=1000, size_bt=None, **kwargs):
+def _deter_bootstrap_size(x: np.ndarray, size_bt: int, max_size_bt: int) -> int:
+    """
+    确定随机抽取样本量
+    
+    Note:
+    -----
+    如果样本量小，则全部代入关联计算; 否则使用max_size对数据进行截断, 减少计算负担
+    """
+    
+    if size_bt is None:
+        if len(x) <= max_size_bt:
+            return len(x)
+        else:
+            logging.warning(
+                f"exec_surrog_indep_test: 采用默认max_size_bt={max_size_bt}作为size_bt, \
+                用于限制代用数据重采样规模")
+            return max_size_bt
+    else:
+        return size_bt
+
+
+def exec_surrog_indep_test(x: np.ndarray, y: np.ndarray, method: str, z: np.ndarray = None, 
+                           xtype: str = None, ytype: str = None, ztype: str = None, rounds: int = 100, 
+                           alpha: float = 0.05, max_size_bt: int = 1000, size_bt: int = None, 
+                           **kwargs) -> Tuple[float, Tuple[float, bool, np.ndarray]]:
     """
     执行基于代用数据的独立性检验
     
     Params:
     -------
-    x: x数据, np.ndarray
-    y: y数据, np.ndarray
-    method: 选用的关联度量方法, str
-    z: z数据, np.ndarray, defaults to None
-    xtype: x的数值类型, str, defaults to None
-    :ytype: y的数值类型, str, defaults to None
-    :ztype: y的数值类型, str, defaults to None
-    rounds: 重复轮数, defaults to 100
-    alpha: 显著性阈值, defaults to 0.05
-    max_size_bt: 用于自助重采样的最大样本数, defaults to 1000
-    size_bt: 自行设定的重采样规模, int, defaults to None
-    kwargs: cal_general_assoc方法中的关键字参数
+    x: x数据
+    y: y数据
+    method: 选用的关联度量方法
+    z: z数据
+    xtype: x的数值类型
+    :ytype: y的数值类型
+    :ztype: y的数值类型
+    rounds: 重复轮数
+    alpha: 显著性阈值
+    max_size_bt: 用于自助重采样的最大样本数
+    size_bt: 自行设定的重采样规模
+    kwargs: cal_general_assoc方法中的关键字参数, 见coefficient.__init__.cal_general_assoc
     
     Returns:
     --------
@@ -51,31 +75,25 @@ def exec_surrog_indep_test(x, y, method, z=None, xtype=None, ytype=None, ztype=N
     assocs_srg: 代理数据关联值数组, np.ndarray
     """
     
-    # 如果样本量小，则全部代入关联计算; 否则使用max_size对数据进行截断, 减少计算负担
-    if size_bt is None:
-        if len(x) <= max_size_bt:
-            size_bt = len(x)
-        else:
-            logging.warning(
-                f"exec_surrog_indep_test: 采用默认max_size_bt={max_size_bt}作为size_bt, \
-                用于限制代用数据重采样规模")
-            size_bt = max_size_bt
+    # 确定随机抽样样本量
+    size_bt = _deter_bootstrap_size(x, size_bt, max_size_bt)
         
     # 计算关联系数
     # TODO: 通过随机抽样获得关联值分布, 有放回or无放回?
     if z is None:
-        assoc = cal_general_assoc(x[:size_bt], y[:size_bt], z, method, xtype, ytype, ztype, **kwargs)
+        assoc = cal_general_assoc(x[:size_bt], y[:size_bt], None, method, xtype, ytype, ztype, **kwargs)
     else:
         assoc = cal_general_assoc(x[:size_bt], y[:size_bt], z[:size_bt], method, xtype, ytype, ztype, **kwargs)
 
     # 计算背景值
     idxs = np.arange(len(x))
     assocs_srg = np.array([])
+    
     for _ in range(rounds):
         # 基于代用数据获得背景值
-        idxs_bt = random.choices(idxs, k=size_bt)  # NOTE: 有放回抽样
+        idxs_bt = random.choices(idxs, k=size_bt)   # 有放回抽样
         x_bt, y_bt = x[idxs_bt], y[idxs_bt]
-        x_srg = np.random.permutation(x_bt)  # 随机重排获得代用数据
+        x_srg = np.random.permutation(x_bt)         # 随机重排获得代用数据
         
         if z is None:
             assoc_srg = cal_general_assoc(x_srg, y_bt, None, method, xtype, ytype, ztype, **kwargs)
@@ -85,7 +103,7 @@ def exec_surrog_indep_test(x, y, method, z=None, xtype=None, ytype=None, ztype=N
 
         assocs_srg = np.append(assocs_srg, assoc_srg)
 
-    # 计算显著性
+    # 计算显著性: 单侧检验
     p = len(assocs_srg[assocs_srg >= assoc]) / rounds
     indep = p >= alpha
     
---
+++ test/independence_test/test_surrog_indep_test.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2023/04/26 17:19:33
+
+@File -> test_surrog_indep_test.py
+
+@Author: luolei
+
+@Email: dreisteine262@163.com
+
+@Describe: 基于代用数据的独立性检验
+"""
+
+import arviz as az
+import numpy as np
+import sys
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), "../" * 3))
+sys.path.insert(0, BASE_DIR)
+
+from setting import plt
+from giefstat.indepence_test.surrog_indep_test import exec_surrog_indep_test
+from dataset.bivariate.data_generator import DataGenerator
+
+
+def gen_test_data(func, N, scale):
+    data_gener = DataGenerator()
+    x, y, _, _ = data_gener.gen_data(N, func, normalize=False)
+    y_range = np.max(y) - np.min(y)
+    noise = np.random.uniform(-scale * y_range, scale * y_range, y.shape)
+    y_noise = y.copy() + noise
+    return x, y_noise
+
+
+if __name__ == "__main__":
+    # 数据生成
+    x, y = gen_test_data("sin_low_freq", 1000, 0.5)
+    
+    plt.scatter(x, y, color="w", edgecolor="k")
+    
+    # 随机测试参数设置
+    rounds = 100
+    alpha = 0.05
+    
+    # 二元变量独立性检验
+    method = "MI-GIEF"
+    assoc, (p, indep, assocs_srg) = exec_surrog_indep_test(
+        x, y, method, xtype="c", ytype="c", rounds=rounds, alpha=alpha)
+    
+    az.plot_posterior(
+        {f"{method}_Surrog": assocs_srg}, 
+        kind="hist", 
+        bins=20, 
+        ref_val=assoc, 
+        hdi_prob=1 - alpha * 2)
+    
+    # 三元变量独立性检验
+    method = "CMI-GIEF"
+    assoc, (p, indep, assocs_srg) = exec_surrog_indep_test(
+        x, y, method, z=x.copy(), xtype="c", ytype="c", ztype="c", rounds=rounds, alpha=alpha)
+    
+    az.plot_posterior(
+        {f"{method}_Surrog": assocs_srg}, 
+        kind="hist", 
+        bins=20, 
+        ref_val=assoc, 
+        hdi_prob=1 - alpha * 2)
